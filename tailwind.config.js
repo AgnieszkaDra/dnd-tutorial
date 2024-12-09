caa4af060ba6fb743a8e5f6740c13561d8783ccf
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-12-09 05:15:25 +0100

    learning

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"
+];
+export const theme = {
+  extend: {
+      colors: {
+        "mainBackgroundColor": '#0D1117',
+        "columnBackgroundColor": '#161C22' 
+    },
+  },
+};
+export const plugins = [];
+
